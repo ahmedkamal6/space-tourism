4b4d10b5650c50a54288610d5842aea11ebb7de3
ahmedkamal6 <87461881+ahmedkamal6@users.noreply.github.com>
2022-10-04 02:19:45 -0700

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+$(function(){
+    if($(window).width() <= 768 )
+            $("br").hide();
+    $(window).resize(function () { 
+        if($(window).width() <= 768 )
+            $("br").hide();
+        else
+            $("br").show();    
+    });
+    $(".burger").on('click', function () {
+        $(".mobNav").slideToggle(500);
+        if($(".burger img").attr("src") != "../assets/shared/icon-close.svg")
+            $(".burger img").attr("src", "../assets/shared/icon-close.svg");
+        else
+        {
+            $(".burger img").fadeOut(250);
+            setTimeout(function () {  
+            $(".burger img").attr("src",  "../assets/shared/icon-hamburger.svg");
+            $(".burger img").fadeIn(1);
+            },200);
+        }
+    });
+
+    function executeIfMinWidth768 (e) {
+        if (window.matchMedia('(min-width: 768px)').matches) {
+          $(".mobNav").hide();
+        }
+      }
+      
+      // call initially
+      executeIfMinWidth768();
+      
+      // add handler for resize
+      window.addEventListener('resize', executeIfMinWidth768);
+});
